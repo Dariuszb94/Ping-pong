87ef42a12ee9e048e8948f9a96b8e322b792f1f9
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-07-17 21:12:58 +0200

    square to player

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Player = () => {
-  return <></>;
+  return (
+    <>
+      <Square></Square>
+    </>
+  );
 };
 
 export default Player;
+
+const Square = styled.div``;
