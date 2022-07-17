94c487d228d402f3fbe713c9e649d6eed771b621
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-07-17 21:22:21 +0200

    add event to useEffect

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Pawn } from './PlayerStyles';
 
 const Player = () => {
+  const handleKeyUpDown = () => {};
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyUpDown, false);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyUpDown, false);
+    };
+  }, []);
   return (
     <>
       <Pawn></Pawn>
