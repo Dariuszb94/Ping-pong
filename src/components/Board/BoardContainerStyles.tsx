c05d67725045318dabc1fc27089ee7a38efd4366
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-07-22 13:18:10 +0200

    board full height

--- src/components/Board/BoardContainerStyles.tsx
+++ src/components/Board/BoardContainerStyles.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Board = styled.div`
   position: relative;
-  padding: 30px;
+  margin: 30px;
   border: 1px solid black;
+  height: calc(100vh - 60px);
+  box-sizing: border-box;
 `;
