9281ad0a76d6c910ad053bed08ac356c3d0c16db
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-07-22 13:24:15 +0200

    pawn pass position

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -24,7 +24,7 @@ const Player = () => {
 
   return (
     <>
-      <Pawn></Pawn>
+      <Pawn position={position}></Pawn>
     </>
   );
 };
--- src/components/Player/PlayerStyles.tsx
+++ src/components/Player/PlayerStyles.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
-export const Pawn = styled.div`
+export const Pawn = styled.div<{ position: number }>`
   height: 200px;
   width: 40px;
   background-color: black;
   position: absolute;
   left: 0;
+  top: ${(props) => props.position};
 `;
