8f9c329ce2da5b1c10dc4664c512167b297d34d6
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-07-25 18:55:25 +0200

    board margins

--- src/components/Board/BoardContainerStyles.tsx
+++ src/components/Board/BoardContainerStyles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Board = styled.section`
   width: 100%;
   position: relative;
-  margin: 30px 100px;
+  margin: 30px;
   border: 1px solid black;
   height: calc(100vh - 60px);
   box-sizing: border-box;
