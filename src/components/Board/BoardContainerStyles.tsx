45615b93f96a401fc5109548228d6d77422880d9
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-07-25 19:16:22 +0200

    feat: board desktop margin

--- src/components/Board/BoardContainerStyles.tsx
+++ src/components/Board/BoardContainerStyles.tsx
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 export const Board = styled.section`
   width: 100%;
   position: relative;
-  margin-left: 30px;
   border: 1px solid black;
   height: calc(100vh - 40px);
   box-sizing: border-box;
+  @media (min-width: 1001px) {
+    margin-left: 30px;
+  }
   @media (max-width: 1000px) {
     height: calc(100vh - 200px);
   }
