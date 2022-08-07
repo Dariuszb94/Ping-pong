b53ae86cd7ec50a12d09f22452befb1c2ed9dfe3
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-08-07 19:39:23 +0200

    styles: board boarders

--- src/components/Board/BoardContainerStyles.tsx
+++ src/components/Board/BoardContainerStyles.tsx
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 export const Board = styled.section`
   width: 100%;
   position: relative;
-  border: 1px solid black;
+  border-bottom: 12px solid black;
+  border-top: 12px solid black;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
   height: calc(100vh - 40px);
   box-sizing: border-box;
   @media (min-width: 1001px) {
