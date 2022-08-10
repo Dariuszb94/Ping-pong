7413cc031676c51ac05d5b03506d14a74f74077b
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2022-08-10 16:27:12 +0200

    refactor: delete context

--- src/context/GameOverContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-import { IGameOver } from '../types/types';
-
-export const defaultGameOver: IGameOver = { isGameOver: false, winner: '' };
-export const GameOverContext = createContext(defaultGameOver);
